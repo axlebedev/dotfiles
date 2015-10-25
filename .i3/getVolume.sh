8283408aabba98654f82e72f4af2333b24fe7291
alexey-broadcast <alexey.broadcast@gmail.com>
2015-10-25 23:15:44 +0300

    [*] .conkyrc: muted volume now has its own icon

--- .i3/getVolume.sh
+++ .i3/getVolume.sh
@@ -1,1 +1,1 @@
-amixer get Master | awk -F'[]%[]' '/%/ {if ($7 == "off") { print "Mute" } else { print $2"%" }}'
+amixer get Master | awk -F'[]%[]' '/%/ {if ($7 == "off") { print "0" } else { print $2"%" }}'
