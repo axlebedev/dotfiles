f32fdb82854e12437f0ac5be31b143029eb3df5e
Alex Lebedev <alevedev@biarum.com>
2016-10-02 00:14:48 +0300

    [+] i3: cinnamon-sound-applet

---
+++ .i3/switchLauout.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#
+# Quickly switch between a given keyboard layout and the US Qwerty one
+
+OTHER_LAYOUT="ru"
+CURRENT="$(setxkbmap -query | grep layout | perl -pe 's/^layout: +([^ ]+)$/$1/')"
+
+if [ "$CURRENT" = "us" ] ; then
+    setxkbmap -layout "$OTHER_LAYOUT" -option ctrl:nocaps
+else
+    setxkbmap -layout us -option ctrl:nocaps
+fi
