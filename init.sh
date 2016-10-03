46cec4ddd9ca8406278690b725a05bdede800fae
Alex Lebedev <alevedev@biarum.com>
2016-10-03 23:42:19 +0300

    [+] desktop entry for skype-web

--- init.sh
+++ init.sh
@@ -2,18 +2,28 @@
 # create symlinks
 rm ~/.conkyrc
 ln -s ~/dotfiles/.conkyrc ~/.conkyrc
+
 rm ~/.bashrc
 ln -s ~/dotfiles/.bashrc ~/.bashrc
+
 rm ~/.ctags
 ln -s ~/dotfiles/.ctags ~/.ctags
+
 rm ~/.eslintrc
 ln -s ~/dotfiles/.eslintrc ~/.eslintrc
+
 rm ~/.gitconfig
 ln -s ~/dotfiles/.gitconfig ~/.gitconfig
+
 rm ~/.vimrc
 ln -s ~/dotfiles/.vimrc ~/.vimrc
+
 rm ~/.i3
 ln -s ~/dotfiles/.i3 ~/.i3
+
+mkdir ~/.config/fish/
 rm ~/.config/fish/config.fish
 ln -s ~/dotfiles/fish/config.fish ~/.config/fish/config.fish
+
+cp ~/dotfiles/desktop/skypeweb.desktop ~/.local/share/applications/
 
