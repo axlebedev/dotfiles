28a901de24348e5ed3f09d28ce805d0a8f927278
Alex Lebedev <alebedev@spbfiller.com>
2020-04-10 00:59:40 +0300

    [+] fish: <C-g> to fuzzy-find branch, sorted by reflog, then all others

---
+++ fish/sortedBranch.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# allBranches=""
+# while read -r line 
+# do
+#     allBranches=$"${allBranches}\n${line}"
+# done < $(git branch --all |\
+#     grep -v HEAD |\
+#     sed -r 's/(remotes\/)(origin\/)(.*)/\2\3\n\3/' |\
+#     sed -r 's/\* //')
+# # printf "%s" $allBranches
+
+allBranches="$(git branch --all |\
+    grep -v HEAD |\
+    sed -r 's/(remotes\/)(origin\/)(.*)/\2\3\n\3/' |\
+    sed -r 's/\* //')"
+
+localBranches="$(git log -g --grep-reflog "checkout:" --format="%gs" |\
+    cat -n |\
+    sed -E "s/^\s+([0-9]+).*from (.*) to .*/\2/g" |\
+    grep -v --regexp="[0-9a-f]\{40\}\$")"
+printf "$localBranches \n $allBranches" | sed -r 's/^ *(.*) *$/\1/' | awk '!x[$0]++'
+
+# printf "%s%s" $localBranches $allBranches | fzf --no-sort -i --reverse --height=50%
