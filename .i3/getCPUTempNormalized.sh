a41b98a49007d0235cedb743ac317a793f92e124
Alex <alexey.broadcast@gmail.com>
2021-12-06 00:10:02 +0300

    [*] conky: brush with new styles

--- .i3/getCPUTempNormalized.sh
+++ .i3/getCPUTempNormalized.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # map 30 - 70 degrees to 0-100
-CPUTEMP=$(cat /sys/devices/platform/coretemp.0/hwmon/hwmon1/temp1_input | cut -c-2)
+CPUTEMP=$(cat /sys/devices/platform/coretemp.0/hwmon/hwmon2/temp1_input | cut -c-2)
 echo $(($CPUTEMP * 5 / 2 - 75))
