5104b057de690fca090cb9e547c2596b69cff14c
Alex <alexey.broadcast@gmail.com>
2021-12-17 13:37:58 +0300

    [*] i3 conky: skip all non LMB events

--- .i3/i3status.sh
+++ .i3/i3status.sh
@@ -15,6 +15,14 @@ do
     fi
 
     line=`echo $line | sed "s/^,//"`
+
+    # 1 - left, 2 - middle, 3 - right, 4 - wheel up, 5 - wheel down
+    button=`echo $line | jshon -e button -u`
+    if [ $button != "1" ]
+    then
+        continue
+    fi
+
     getname=`echo $line | jshon -e name -u`
 
     if [ "$getname" = "Date" ] 
