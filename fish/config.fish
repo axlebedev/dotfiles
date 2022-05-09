fb6354cbd24980d98a02d7a22f5062092f4daad3
Alex <alexey.broadcast@gmail.com>
2022-05-09 12:18:03 +0300

    [-] fish: select from prev output. Remove because it re-runs last command

--- fish/config.fish
+++ fish/config.fish
@@ -116,12 +116,3 @@ abbr -a ys yarn start
 abbr -a ysn yarn snfiller
 abbr -a ycc yarn cache clean
 abbr -a yi yarn --ignore-engines
-
-function __select_from_last
-	set -l FZF_OUT (eval $history[1] | fzf)
-	if test -n "$FZF_OUT"
-		commandline -a "$FZF_OUT "
-		commandline -f end-of-line
-	end
-end
-bind \cn __select_from_last
