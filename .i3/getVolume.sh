16fef6102b12ac5ef5a39a7ac325347fd2fc7be5
Alex Lebedev <alexey.broadcast@gmail.com>
2023-11-30 22:01:12 +0300

    [*] geetVolume.sh: different view for muted and volume=0

--- .i3/getVolume.sh
+++ .i3/getVolume.sh
@@ -1,2 +1,10 @@
 # amixer -D pulse sget Master | awk -F'[]%[]' '/%/ {if ($7 == "off") { print "0" } else { print $2 }}'
-amixer -D pulse sget Master | tail -1 | sed -r -e 's/.+\[([0-9]+)%.+/\1/'
+VOLUME=$(amixer -D pulse sget Master | tail -1 | sed -r -e 's/.+\[([0-9]+)%.+/\1/')
+MUTED=$(amixer -D pulse sget Master | tail -1 | sed -r -e 's/.+(on|off).*$/\1/')
+
+if [ $MUTED = "off" ];
+then
+    echo "Muted"
+else 
+    echo $VOLUME
+fi
