ce1491a869b7b5ce74cd53136c66c0afb5a38f52
Alex Lebedev <alexey.broadcast@gmail.com>
2024-12-01 19:45:48 +0300

    [+] init.sh: add .vim/pack Also make all 'rm' => 'rf -rf'

--- init.sh
+++ init.sh
@@ -3,41 +3,44 @@
 mkdir -p ~/chrome-users
 
 # create symlinks
-rm ~/.conkyrc
+rm -rf ~/.conkyrc
 ln -s ~/dotfiles/conky/.conkyrc ~/.conkyrc
 
-rm ~/.vim/coc-settings.json
-ln -s ~/dotfiles/.vim/coc-settings.json ~/.vim/coc-settings.json
-
-rm ~/.bashrc
+rm -rf ~/.bashrc
 ln -s ~/dotfiles/.bashrc ~/.bashrc
 
-rm ~/.ctags
+rm -rf ~/.ctags
 ln -s ~/dotfiles/.ctags ~/.ctags
 
-rm ~/.eslintrc
+rm -rf ~/.eslintrc
 ln -s ~/dotfiles/.eslintrc ~/.eslintrc
 
-rm ~/.gitconfig
+rm -rf ~/.gitconfig
 ln -s ~/dotfiles/.gitconfig ~/.gitconfig
 
-rm ~/.vimrc
+rm -rf ~/.vimrc
 ln -s ~/dotfiles/.vimrc ~/.vimrc
 
-rm ~/.vim/ftplugin
+rm -rf ~/.vim/ftplugin
 ln -s ~/dotfiles/.vim/ftplugin ~/.vim/ftplugin
 
-rm ~/.i3
+rm -rf ~/.vim/coc-settings.json
+ln -s ~/dotfiles/.vim/coc-settings.json ~/.vim/coc-settings.json
+
+rm -rf ~/.vim/pack
+ln -s ~/dotfiles/.vim/pack ~/.vim/pack
+
+rm -rf ~/.i3
 ln -s ~/dotfiles/.i3 ~/.i3
 
-rm ~/.fzf.bash
+rm -rf ~/.fzf.bash
 ln -s ~/dotfiles/.fzf.bash ~/.fzf.bash
 
-rm ~/.config/dunst
+rm -rf ~/.config/dunst
 ln -s ~/dotfiles/dunst ~/.config/dunst
 
 mkdir ~/.config/fish/
-rm ~/.config/fish/config.fish
+rm -rf ~/.config/fish/config.fish
 ln -s ~/dotfiles/fish/config.fish ~/.config/fish/config.fish
 
 cp ~/dotfiles/desktop/skypeweb.desktop ~/.local/share/applications/
