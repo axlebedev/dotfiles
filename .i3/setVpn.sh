56092b2791f774de80d867898e2f675031456e43
Alex Lebedev <alexey.broadcast@gmail.com>
2024-12-01 19:45:16 +0300

    [*] i3: fix path for new 'anyconnect' client

--- .i3/setVpn.sh
+++ .i3/setVpn.sh
@@ -3,7 +3,7 @@ HAS_VPN="$(bash ~/dotfiles/.i3/getVPN.sh)"
 echo $HAS_VPN
 if [ $HAS_VPN = "VPN" ]
 then
-    /opt/cisco/anyconnect/bin/vpn -s disconnect
+    /opt/cisco/secureclient/bin/vpn -s disconnect
 else
     notify-send --expire-time=3000 "Try to VPN. Approve at 'Multifactor' mobile app"
     printf '\n\nPASSWORD\n' | /opt/cisco/anyconnect/bin/vpn -s connect vpn2.ncloudtech.ru
