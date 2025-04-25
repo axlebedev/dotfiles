cd85bdf0b74bf0705e0e2c0f8fcb340fea079039
Alex Lebedev <alexey.broadcast@gmail.com>
2025-04-25 22:51:05 +0300

    [*] setVpn: fix

--- .i3/setVpn.sh
+++ .i3/setVpn.sh
@@ -6,5 +6,5 @@ then
     /opt/cisco/secureclient/bin/vpn -s disconnect
 else
     notify-send --expire-time=3000 "Try to VPN. Approve at 'Multifactor' mobile app"
-    printf '\n\nPASSWORD\n2\n' | /opt/cisco/secureclient/bin/vpn -s connect vpn2.ncloudtech.ru
+    printf '\n\NPASSWORD\n' | /opt/cisco/secureclient/bin/vpn -s connect vpn2.ncloudtech.ru
 fi
