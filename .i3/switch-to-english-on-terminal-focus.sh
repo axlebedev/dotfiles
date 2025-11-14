5178f7af81606d43e277310056742e8d2ecdc0b8
Alex Lebedev <alexey.broadcast@gmail.com>
2025-11-14 16:36:00 +0300

    [*] switch-to-english-on-terminal-focus.sh: make it work without xkb-switch

--- .i3/switch-to-english-on-terminal-focus.sh
+++ .i3/switch-to-english-on-terminal-focus.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
 doSwitchToEnglishOnTerminalFocus() {
-    CLASS=$(echo $1 | jq ".container.window_properties.class")
+    CLASS=$(echo $1 | jq -r ".container.window_properties.class" 2>/dev/null || echo "")
 
-    if [ $CLASS = "\"Gnome-terminal\"" ]; then
-        xkb-switch -s us
+    if [ "$CLASS" = "Gnome-terminal" ]; then
+        gsettings set org.gnome.desktop.input-sources current 0
+        # строка выше - то же самое что xkb-switch -s us
     fi
 }
 
-i3-msg -t subscribe -m '[ "window" ]' | while read line ; do doSwitchToEnglishOnTerminalFocus $line; done
+i3-msg -t subscribe -m '[ "window" ]' | while read -r line ; do 
+    doSwitchToEnglishOnTerminalFocus "$line"; 
+done
+
