6c1385b88a157f37d90a17da87f253368c777299
Alex Lebedev <alexey.broadcast@gmail.com>
2025-11-29 09:23:29 +0300

    [*] getVpn.sh: fix for new system and vpn

--- .i3/getVPN.sh
+++ .i3/getVPN.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # ACTIVE_CON="$(nmcli -f NAME,ACTIVE,TYPE con | grep --perl-regexp "yes\s+vpn")"
-ACTIVE_CON="$(cat /proc/net/route | grep --perl-regexp "ppp|tun")"
+# ACTIVE_CON="$(cat /proc/net/route | grep --perl-regexp "ppp|tun")"
+ACTIVE_CON="$(nmcli -c no con show "WB" | grep STATE:)"
+
 
 if [[ $ACTIVE_CON = "" ]];
 then
