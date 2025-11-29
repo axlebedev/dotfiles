b8b49b548bc2b758309456e3e910b3122d95e7c8
Alex Lebedev <alexey.broadcast@gmail.com>
2025-11-29 09:24:26 +0300

    [*] setVpn: use nmcli

--- .i3/setVpn.sh
+++ .i3/setVpn.sh
@@ -3,8 +3,11 @@ HAS_VPN="$(bash ~/dotfiles/.i3/getVPN.sh)"
 echo $HAS_VPN
 if [ $HAS_VPN = "VPN" ]
 then
-    /opt/cisco/secureclient/bin/vpn -s disconnect
+    # /opt/cisco/secureclient/bin/vpn -s disconnect
+    nmcli con down "WB"
 else
-    notify-send --expire-time=3000 "Try to VPN. Approve at 'Multifactor' mobile app"
-    printf '\n\NPASSWORD\n' | /opt/cisco/secureclient/bin/vpn -s connect vpn2.ncloudtech.ru
+    nmcli con up "WB"
+
+    # notify-send --expire-time=3000 "Try to VPN. Approve at 'Multifactor' mobile app"
+    # printf '\n\NPASSWORD\n' | /opt/cisco/secureclient/bin/vpn -s connect vpn2.ncloudtech.ru
 fi
