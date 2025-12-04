d9bce47f044315323e843a1208bf10e4e41bab17
Alex Lebedev <alexey.broadcast@gmail.com>
2025-12-04 20:40:21 +0300

    [*] switch-layout: make it work (was bad)

--- .i3/switchLauout.sh
+++ .i3/switchLauout.sh
@@ -2,11 +2,19 @@
 #
 # Quickly switch between a given keyboard layout and the US Qwerty one
 
-OTHER_LAYOUT="ru"
-CURRENT="$(setxkbmap -query | grep layout | perl -pe 's/^layout: +([^ ]+)$/$1/')"
+# 0 - us, 1 - ru
+if [ $# -eq 1 ]; then
+    TARGET=$1
 
-if [ "$CURRENT" = "us" ] ; then
-    setxkbmap -layout "$OTHER_LAYOUT" -option ctrl:nocaps
+    if [[ $1 == "en" ]]; then
+        gsettings set org.gnome.desktop.input-sources current 1
+    else
+        gsettings set org.gnome.desktop.input-sources current 0
+    fi
 else
-    setxkbmap -layout us -option ctrl:nocaps
+    CURRENT="$(gsettings get org.gnome.desktop.input-sources current | awk '{ print $2 }')"
+
+    gsettings set org.gnome.desktop.input-sources current $((1 - CURRENT))
 fi
+
+
