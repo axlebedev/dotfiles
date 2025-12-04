65a2464541fc633f5dfa0376979f1557ac31c25b
Alex Lebedev <alexey.broadcast@gmail.com>
2025-12-04 20:39:08 +0300

    [*] switch-to-english-on-terminal-focus.sh: listen also 'kitty'

--- .i3/switch-to-english-on-terminal-focus.sh
+++ .i3/switch-to-english-on-terminal-focus.sh
@@ -3,7 +3,7 @@
 doSwitchToEnglishOnTerminalFocus() {
     CLASS=$(echo $1 | jq -r ".container.window_properties.class" 2>/dev/null || echo "")
 
-    if [ "$CLASS" = "Gnome-terminal" ]; then
+    if [ "$CLASS" = "Gnome-terminal" ] || [ "$CLASS" = "kitty" ]; then
         gsettings set org.gnome.desktop.input-sources current 0
         # строка выше - то же самое что xkb-switch -s us
     fi
