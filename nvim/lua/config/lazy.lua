50af47f4633e59a4eb0a9335cc6b251a1c3c3743
Alex Lebedev <alexey.broadcast@gmail.com>
2026-01-15 17:31:32 +0300

    another bunch of plugins

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -24,13 +24,13 @@ vim.g.maplocalleader = '\\'
 -- Setup lazy.nvim
 require('lazy').setup({
   spec = {
-    { 'sheerun/vim-polyglot', lazy = true },
+    { 'sheerun/vim-polyglot' },
 
     -- Automatically create parent directories on write when don't exist already.
-    { 'pbrisbin/vim-mkdir', lazy = true },
+    { 'pbrisbin/vim-mkdir' },
 
     -- Make '.' work on plugin commands (not all maybe)
-    { 'tpope/vim-repeat', lazy = true }, -- TODO: nnoremap <Plug>(RepeatRedo) U
+    { 'tpope/vim-repeat' }, -- TODO: nnoremap <Plug>(RepeatRedo) U
 
     -- Start screen for vim
     {
@@ -45,10 +45,97 @@ require('lazy').setup({
       end
     },
 
-  },
+    -- NERDTree
+    {
+      'nvim-tree/nvim-tree.lua', -- NvimTreeToggle
+      dependencies = { 'nvim-tree/nvim-web-devicons' },
+      config = function()
+        require('nvim-tree').setup {
+          view = { width = 35, side = 'left' },
+          filters = { dotfiles = false },  -- Show hidden files (like NERDTree)
+          git = { enable = true },
+          actions = { open_file = { quit_on_open = false } },
+          renderer = {
+            icons = { glyphs = { default = '', git = { unstaged = '✗' } } },
+            highlight_git = true,
+          },
+        }
 
-  -- Configure any other settings here. See the documentation for more details.
-  -- colorscheme that will be used when installing plugins.
-  install = { colorscheme = { 'habamax' } },
+        -- Keymaps (NERDTree-like)
+        vim.keymap.set('n', '<C-n>', ':NvimTreeToggle<CR>')
+        vim.keymap.set('n', '<leader>e', ':NvimTreeFocus<CR>')
+      end,
+    },
+
+    -- fzf
+    {
+      'nvim-telescope/telescope.nvim',
+      dependencies = {
+        'nvim-lua/plenary.nvim',
+        -- optional but recommended
+        { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+      }
+    },
+
+    -- Markdown
+    -- NOTE: need installed 'yarn': 'sudo npm i -g yarn'
+    {
+      "iamcco/markdown-preview.nvim",
+      cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+      build = "cd app && yarn install",
+      init = function()
+        vim.g.mkdp_filetypes = { "markdown" }
+      end,
+      ft = { "markdown" },
+    },
+
+    -- open browser
+    {
+      'tyru/open-browser.vim',
+      keys = {
+        { '<F3>', '<Plug>(openbrowser-smart-search)', mode = 'n' },
+        { '<F3>', '<Plug>(openbrowser-smart-search)', mode = 'v' },
+      },
+    },
+
+    -- bufonly
+    { 'schickling/vim-bufonly' },
+
+    -- clear hlsearch on cursor move
+    {
+      'nvimdev/hlsearch.nvim',
+      event = 'BufRead',
+      config = function()
+        require('hlsearch').setup()
+      end
+    },
+
+    -- color scheme
+    {
+      'NLKNguyen/papercolor-theme',
+      priority = 1000, -- ensures the colorscheme loads first
+    },
+
+    -- highlight 'f' character
+    {
+      "rhysd/clever-f.vim",
+      config = function()
+        vim.g.clever_f_smart_case = 1
+        vim.g.clever_f_across_no_line = 1
+        vim.keymap.set("n", ";", "<Plug>(clever-f-repeat-forward)", { noremap = false })
+      end,
+    },
+
+    -- vim-smoothie
+    {
+      "karb94/neoscroll.nvim",
+      opts = {
+        mappings = { '<C-u>', '<C-d>', '<C-b>', '<C-f>', 'zt', 'zz', 'zb', },
+      },
+    }
+  },
 })
 
+vim.opt.termguicolors = true
+vim.opt.background = 'light'
+vim.cmd('colorscheme PaperColor')
