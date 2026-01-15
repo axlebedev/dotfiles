81931e7b08049f280d213a9beaa038544e5b104b
Alex Lebedev <alexey.broadcast@gmail.com>
2026-01-15 22:28:21 +0300

    another bunch of plugins

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -21,6 +21,7 @@ vim.opt.rtp:prepend(lazypath)
 vim.g.mapleader = ' '
 vim.g.maplocalleader = '\\'
 
+vim.opt.mousemoveevent = true
 -- Setup lazy.nvim
 require('lazy').setup({
   spec = {
@@ -80,13 +81,13 @@ require('lazy').setup({
     -- Markdown
     -- NOTE: need installed 'yarn': 'sudo npm i -g yarn'
     {
-      "iamcco/markdown-preview.nvim",
-      cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-      build = "cd app && yarn install",
+      'iamcco/markdown-preview.nvim',
+      cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
+      build = 'cd app && yarn install',
       init = function()
-        vim.g.mkdp_filetypes = { "markdown" }
+        vim.g.mkdp_filetypes = { 'markdown' }
       end,
-      ft = { "markdown" },
+      ft = { 'markdown' },
     },
 
     -- open browser
@@ -118,21 +119,193 @@ require('lazy').setup({
 
     -- highlight 'f' character
     {
-      "rhysd/clever-f.vim",
+      'rhysd/clever-f.vim',
       config = function()
         vim.g.clever_f_smart_case = 1
         vim.g.clever_f_across_no_line = 1
-        vim.keymap.set("n", ";", "<Plug>(clever-f-repeat-forward)", { noremap = false })
+        vim.keymap.set('n', ';', '<Plug>(clever-f-repeat-forward)', { noremap = false })
       end,
     },
 
     -- vim-smoothie
     {
-      "karb94/neoscroll.nvim",
+      'karb94/neoscroll.nvim',
       opts = {
         mappings = { '<C-u>', '<C-d>', '<C-b>', '<C-f>', 'zt', 'zz', 'zb', },
       },
-    }
+    },
+
+    -- airline
+    {
+      'nvim-lualine/lualine.nvim',
+      dependencies = { 'nvim-tree/nvim-web-devicons' },
+      options = {
+        theme = 'papercolor',
+        icons_enabled = true
+      },
+      config = function()
+        local colors = {
+          red = '#ca1243',
+          grey = '#a0a1a7',
+          black = '#383a42',
+          white = '#f3f3f3',
+          light_green = '#83a598',
+          orange = '#fe8019',
+          green = '#8ec07c',
+        }
+
+        local theme = {
+          normal = {
+            a = { fg = colors.white, bg = colors.black },
+            b = { fg = colors.white, bg = colors.grey },
+            c = { fg = colors.black, bg = colors.white },
+            z = { fg = colors.white, bg = colors.black },
+          },
+          insert = { a = { fg = colors.black, bg = colors.light_green } },
+          visual = { a = { fg = colors.black, bg = colors.orange } },
+          replace = { a = { fg = colors.black, bg = colors.green } },
+        }
+
+        local empty = require('lualine.component'):extend()
+        function empty:draw(default_highlight)
+          self.status = ''
+          self.applied_separator = ''
+          self:apply_highlights(default_highlight)
+          self:apply_section_separators()
+          return self.status
+        end
+
+        -- Put proper separators and gaps between components in sections
+        local function process_sections(sections)
+          for name, section in pairs(sections) do
+            local left = name:sub(9, 10) < 'x'
+            for pos = 1, name ~= 'lualine_z' and #section or #section - 1 do
+              table.insert(section, pos * 2, { empty, color = { fg = colors.white, bg = colors.white } })
+            end
+            for id, comp in ipairs(section) do
+              if type(comp) ~= 'table' then
+                comp = { comp }
+                section[id] = comp
+              end
+              comp.separator = left and { right = '' } or { left = '' }
+            end
+          end
+          return sections
+        end
+
+        local function search_result()
+          if vim.v.hlsearch == 0 then
+            return ''
+          end
+          local last_search = vim.fn.getreg('/')
+          if not last_search or last_search == '' then
+            return ''
+          end
+          local searchcount = vim.fn.searchcount { maxcount = 9999 }
+          return last_search .. '(' .. searchcount.current .. '/' .. searchcount.total .. ')'
+        end
+
+        local function modified()
+          if vim.bo.modified then
+            return '+'
+          elseif vim.bo.modifiable == false or vim.bo.readonly == true then
+            return '-'
+          end
+          return ''
+        end
+
+        require('lualine').setup {
+          options = {
+            theme = theme,
+            component_separators = '',
+            section_separators = { left = '', right = '' },
+          },
+          sections = process_sections {
+            lualine_a = { 'mode' },
+            lualine_b = {
+              'branch',
+              'diff',
+              {
+                'diagnostics',
+                source = { 'nvim' },
+                sections = { 'error' },
+                diagnostics_color = { error = { bg = colors.red, fg = colors.white } },
+              },
+              {
+                'diagnostics',
+                source = { 'nvim' },
+                sections = { 'warn' },
+                diagnostics_color = { warn = { bg = colors.orange, fg = colors.white } },
+              },
+              { 'filename', file_status = false, path = 1 },
+              { modified, color = { bg = colors.red } },
+              {
+                '%w',
+                cond = function()
+                  return vim.wo.previewwindow
+                end,
+              },
+              {
+                '%r',
+                cond = function()
+                  return vim.bo.readonly
+                end,
+              },
+              {
+                '%q',
+                cond = function()
+                  return vim.bo.buftype == 'quickfix'
+                end,
+              },
+            },
+            lualine_c = {},
+            lualine_x = {},
+            lualine_y = { search_result, 'filetype' },
+            lualine_z = { '%l:%c', '%p%%/%L' },
+          },
+          inactive_sections = {
+            lualine_c = { '%f %y %m' },
+            lualine_x = {},
+          },
+        }
+      end
+    },
+
+    -- bufferline
+    {
+      'akinsho/bufferline.nvim',
+      dependencies = 'nvim-tree/nvim-web-devicons',
+      config = function()
+        require('bufferline').setup({
+          options = {
+            right_mouse_command = false,
+            tab_size = 3,
+            max_name_length = 100,
+            name_formatter = function(buf)
+              if string.find(buf.name, 'index') then
+                tail = buf.path:sub(1, buf.path:len() - buf.name:len())
+                return tail:match("([^/]+)/$") .. '/' .. buf.name
+              end
+              return buf.name
+            end,
+            offsets = {
+              {
+                filetype = "NvimTree",
+                text = "File Explorer",
+                text_align = "left",
+                separator = true,
+              }
+            },
+            separator_style = 'slant',
+            hover = {
+              enabled = true,
+              delay = 150,
+              reveal = {'close'}
+            }
+          }
+        })
+      end,
+    },
   },
 })
 
