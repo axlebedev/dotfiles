01f1037afd29510d42cf773ef4e667613da728cc
Alex Lebedev <alexey.broadcast@gmail.com>
2026-01-25 17:51:54 +0300

    opennextbuf; fix togglequickfix

---
+++ nvim/lua/opennextbuf.lua
@@ -0,0 +1,14 @@
+-- Skip quickfix on traversing buffers
+local M = {}
+local openNextBufInner = function(isPrev)
+    local command = isPrev and "bprev" or "bnext"
+    vim.cmd(command)
+    if vim.bo.buftype == 'quickfix' or vim.bo.buftype == 'quickfix' then
+        M.openNextBuf(isPrev)
+    end
+end
+
+M.openNextBuf = function() openNextBufInner(false) end
+M.openPrevBuf = function() openNextBufInner(true) end
+
+return M
