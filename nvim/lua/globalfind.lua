9dd3ffd440fa2e4203b8c0b6240210b7b27104de
Alex Lebedev <alexey.broadcast@gmail.com>
2026-01-26 18:21:08 +0300

    globalfind (step 2) filterTestEntries

--- nvim/lua/globalfind.lua
+++ nvim/lua/globalfind.lua
@@ -125,4 +125,40 @@ M.Grep = function()
     vim.keymap.del('c', '<C-i>')
 end
 
+local filtered = {
+    'node_modules',
+    'json',
+    'test__',
+    'git',
+    'diff',
+    'commonMock',
+    'yarn.lock',
+    'fake-api',
+    'api-types',
+    'crowdin',
+    'localization',
+    'dictionaries',
+    'spec.ts',
+    'spec.js',
+    'amrDocs',
+    'amr-docs',
+    'test',
+    'Tests',
+    'QtEditor',
+    '--'
+}
+M.filterTestEntries = function()
+    local list = vim.fn.getqflist()
+    local res = {}
+
+    for _, v in pairs(list) do
+        local bufname = vim.fn.bufname(v.bufnr)
+        if not array.some(filtered, function(v) return v:match(bufname) end) then
+            table.insert(res, v)
+        end
+    end
+    vim.fn.setqflist(res)
+    -- ResizeQFHeight()
+end
+
 return M
