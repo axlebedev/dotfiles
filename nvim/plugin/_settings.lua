bc0ad808a547cc53baf843a7f747e7c5b009f9e6
Alex Lebedev <alexey.broadcast@gmail.com>
2026-02-10 15:32:26 +0300

    nvim-detect-indent; nvim-ufo plugins

--- nvim/plugin/_settings.lua
+++ nvim/plugin/_settings.lua
@@ -183,3 +183,12 @@ vim.api.nvim_create_autocmd({ "FocusLost" }, {
     vim.cmd.wviminfo()
   end,
 })
+
+-- for nvim-ufo {{{
+vim.o.foldcolumn = '1' -- '0' is not bad
+vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+vim.o.foldlevelstart = 99
+vim.o.foldenable = true
+vim.keymap.set('n', 'zR', require('ufo').openAllFolds)
+vim.keymap.set('n', 'zM', require('ufo').closeAllFolds)
+-- }}} for nvim-ufo
