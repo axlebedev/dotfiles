fe661b03954d8daddcb709e8e8aa1e3b4d4e23d8
Alex Lebedev <alexey.broadcast@gmail.com>
2026-02-23 12:36:03 +0300

    Show diagnostic on CursorHold, remove 'coc-nvim' from plugins-migrate2.txt

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -686,17 +686,43 @@ vim.keymap.set('n', '<C-m>', vim.diagnostic.goto_next, { noremap = true, silent 
 vim.keymap.set('n', '<C-n>', vim.diagnostic.goto_prev, { noremap = true, silent = true })
 
 vim.diagnostic.config({
-    signs = {
-      text = {
-        [vim.diagnostic.severity.ERROR] = "●", -- Example: a red circle
-        [vim.diagnostic.severity.WARN]  = "▲", -- Example: a yellow triangle
-        [vim.diagnostic.severity.INFO]  = "i",
-        [vim.diagnostic.severity.HINT]  = "?",
-      },
-      -- You can also configure highlights, etc.
+  signs = {
+    text = {
+      [vim.diagnostic.severity.ERROR] = "●", -- Example: a red circle
+      [vim.diagnostic.severity.WARN]  = "▲", -- Example: a yellow triangle
+      [vim.diagnostic.severity.INFO]  = "i",
+      [vim.diagnostic.severity.HINT]  = "?",
     },
-    update_in_insert = false,
-    -- other diagnostic options...
+    -- You can also configure highlights, etc.
+  },
+  update_in_insert = true,
+  float = {
+    header = false,
+    border = 'rounded',
+  }
+})
+
+vim.api.nvim_create_autocmd({ "CursorHold" }, {
+    pattern = "*",
+    callback = function()
+      for _, winid in pairs(vim.api.nvim_tabpage_list_wins(0)) do
+        if vim.api.nvim_win_get_config(winid).zindex then
+          return
+        end
+      end
+      vim.diagnostic.open_float({
+          scope = "cursor",
+          focusable = false,
+          close_events = {
+            "CursorMoved",
+            "CursorMovedI",
+            "BufHidden",
+            "InsertEnter",
+            "TextChanged",
+            "WinLeave",
+          },
+        })
+    end
   })
 
 -- " Resize to 40 columns (from any window, no focus change)
