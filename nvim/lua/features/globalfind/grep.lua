9c4c12cb881742b399156edb1502ac8bdccb2413
Alex Lebedev <alexey.broadcast@gmail.com>
2026-03-16 18:10:04 +0300

    [*] nvim: globalfind not show duplicate 'filename-line' entries

--- nvim/lua/features/globalfind/grep.lua
+++ nvim/lua/features/globalfind/grep.lua
@@ -104,7 +104,8 @@ M.Grep = function()
         end
         prg = prg .. ' ' .. case
 
-        local output = vim.fn.systemlist(prg .. ' "' .. word .. '" .')
+        local uniqByFilenameLine = "| awk -F: '!seen[$1,$2]++'"
+        local output = vim.fn.systemlist(prg .. ' "' .. word .. '" . ' .. uniqByFilenameLine)
         vim.fn.setqflist({}, ' ', { lines = output })
         vim.cmd.copen()
         resizeQFHeight()
