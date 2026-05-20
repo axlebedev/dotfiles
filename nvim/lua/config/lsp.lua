c0d31a4e8141e0acd497eed4fcebe8a5631692f4
Alex Lebedev <alexey.broadcast@gmail.com>
2026-05-20 12:56:08 +0300

    [+] nvim lsp: add cssls for css, less, sass

--- nvim/lua/config/lsp.lua
+++ nvim/lua/config/lsp.lua
@@ -119,6 +119,16 @@ local init_config = function()
     lineFoldingOnly = true
   }
 
+  vim.lsp.config('cssls', {
+    filetypes = { "css", "scss", "less" },
+    settings = {
+      css = { validate = true },
+      scss = { validate = true },
+      less = { validate = true },
+    }
+  })
+  vim.lsp.enable('cssls')
+
   vim.lsp.config('vtsls', {
       on_attach = function(client, bufnr)
         -- vim.api.nvim_set_hl(0, "LspSigNormal", { fg = "#24273a", bg = "#cad3f5" })
