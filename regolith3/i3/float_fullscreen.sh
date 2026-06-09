17e1a3e91c26a6bae490c16bf9967019f4511208
Alex Lebedev <alexey.broadcast@gmail.com>
2026-06-09 11:23:36 +0300

    [*] i3: new binding "mod+shift+f", separate bar settings for different monitors

--- regolith3/i3/float_fullscreen.sh
+++ regolith3/i3/float_fullscreen.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 i3-msg floating enable
 sleep 0.05
-i3-msg resize set 100 ppt 98 ppt
-i3-msg move position 0 px 158 px
+i3-msg resize set 100 ppt 100 ppt
+# shrink 2 border widths
+i3-msg resize shrink width 4 px
+# shrink 2 border heights + statusbar height
+i3-msg resize shrink height 32 px
+i3-msg move position 0 0
+
