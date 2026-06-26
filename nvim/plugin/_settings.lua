90c5b13e7457546db92400ef15a9080060335e52
Alex Lebedev <alexey.broadcast@gmail.com>
2026-06-26 23:13:51 +0300

    [*] nvim: return old foldmethod

--- nvim/plugin/_settings.lua
+++ nvim/plugin/_settings.lua
@@ -89,8 +89,7 @@ vim.opt.splitright = true
 -- Фолдинг
 vim.opt.foldenable = true
 
-vim.opt.foldmethod = "expr"
-vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+vim.opt.foldmethod = "syntax"
 
 vim.opt.foldlevel = 99
 vim.opt.fillchars = {
